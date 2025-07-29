be124a9d54faa6131f0d9c2158190c3b348695a5
Ian Cardona <iancardona.dev@gmail.com>
2025-07-29 21:06:59 +0800

    feat(codetask): add codetask model
    - Add findByUserId, update, and delete functions in codetask model
    
    - Create server.ts file
    
    - Rename index.ts to app.ts

--- packages/api/src/models/codetask.model.ts
+++ packages/api/src/models/codetask.model.ts
@@ -1,4 +1,9 @@
-import { PutCommand } from '@aws-sdk/lib-dynamodb';
+import {
+  DeleteCommand,
+  PutCommand,
+  QueryCommand,
+  UpdateCommand,
+} from '@aws-sdk/lib-dynamodb';
 import { docClient } from '../config/dynamodb';
 import { CodeTask } from '../types/codetask.type';
 
@@ -14,5 +19,47 @@ export const CodeTaskModel = {
       })
     );
     return task;
+  },
+
+  async findByUserId(userId: string): Promise<CodeTask[]> {
+    const result = await docClient.send(
+      new QueryCommand({
+        TableName: CODE_TASK_TABLE,
+        KeyConditionExpression: 'userId = :userId',
+        ExpressionAttributeValues: { ':userId': userId },
+      })
+    );
+
+    return result.Items as CodeTask[];
+  },
+
+  async update(id: string, userId: string, updates: Partial<CodeTask>) {
+    const updateExpression: string[] = [];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const expressionAttributeValues: Record<string, any> = {};
+
+    Object.keys(updates).forEach((key, index) => {
+      updateExpression.push(`${key} = :val${index}`);
+      expressionAttributeValues[`:val${index}`] =
+        updates[key as keyof CodeTask];
+    });
+
+    await docClient.send(
+      new UpdateCommand({
+        TableName: CODE_TASK_TABLE,
+        Key: { id, userId },
+        UpdateExpression: `SET ${updateExpression.join(', ')}`,
+        ExpressionAttributeValues: expressionAttributeValues,
+      })
+    );
+  },
+
+  async delete(id: string, userId: string) {
+    await docClient.send(
+      new DeleteCommand({
+        TableName: CODE_TASK_TABLE,
+        Key: { id, userId },
+      })
+    );
   },
 };
