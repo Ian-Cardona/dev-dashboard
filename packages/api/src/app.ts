8249967123f86b33441a63ef0b92a19e186ecb8b
Ian Cardona <iancardona.dev@gmail.com>
2025-08-07 23:27:23 +0800

    refactor: enhance UUID utilities and update app configuration

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import codeTaskRouter from './routes/codetask.route';
+import userRouter from './routes/user.route';
 
 import { errorHandlerMiddleware } from './middlewares/error_handler.middleware';
 import { loggerMiddleware } from './middlewares/logger.middleware';
@@ -12,6 +13,7 @@ app.use(express.json({ limit: '1mb' }));
 app.use(loggerMiddleware);
 
 app.use('/codetasks', codeTaskRouter);
+app.use('/users', userRouter);
 
 app.use(errorHandlerMiddleware);
 
