0f4cba12daf36a6e77bf80da9e7c6238489e55da
Ian Cardona <iancardona.dev@gmail.com>
2025-08-14 22:49:30 +0800

    refactor(auth): add authentication service, authorization middleware, remove obsolete auth files

--- packages/api/src/types/auth.type.ts
+++ packages/api/src/types/auth.type.ts
@@ -1,21 +1,72 @@
-import { ResponseUser } from './user.type';
+import z from 'zod';
+import { JwtPayload } from 'jsonwebtoken';
+import {
+  authenticationRegisterRequestSchema,
+  authenticationLoginRequestSchema,
+  authenticationRefreshRequestSchema,
+  authenticationSuccessResponseSchema,
+  authenticationRefreshResponseSchema,
+} from '../schema/auth.schema';
 
-export type AuthRegisterRequest = {
-  email: string;
-  password: string;
-  firstName?: string;
-  lastName?: string;
-};
+export type AuthenticationRegisterRequest = z.infer<
+  typeof authenticationRegisterRequestSchema
+>;
+export type AuthenticationLoginRequest = z.infer<
+  typeof authenticationLoginRequestSchema
+>;
+export type AuthenticationRefreshRequest = z.infer<
+  typeof authenticationRefreshRequestSchema
+>;
 
-export type AuthRefreshRequest = {
+export type AuthenticationSuccessResponse = z.infer<
+  typeof authenticationSuccessResponseSchema
+>;
+export type AuthenticationRefreshResponse = z.infer<
+  typeof authenticationRefreshResponseSchema
+>;
+
+// JWT Payload
+export interface AuthorizationTokenPayload extends JwtPayload {
   userId: string;
-  refreshToken: string;
-};
-
-export interface AuthSuccessResponse {
-  accessToken: string;
-  refreshToken: string;
-  user: ResponseUser;
+  email: string;
 }
 
-export type AuthRefreshResponse = Omit<AuthSuccessResponse, 'user'>;
+// import { JwtPayload } from 'jsonwebtoken';
+// import { ResponseUser } from './user.type';
+
+// // Authentication
+// export type AuthenticationRegisterRequest = {
+//   email: string;
+//   password: string;
+//   firstName?: string;
+//   lastName?: string;
+// };
+
+// export type AuthenticationLoginRequest = {
+//   email: string;
+//   password: string;
+// };
+
+// export interface AuthenticationSuccessResponse {
+//   accessToken: string;
+//   refreshToken: string;
+//   user: ResponseUser;
+// }
+
+// // Refresh Token
+// export type AuthenticationRefreshRequest = {
+//   userId: string;
+//   refreshToken: string;
+// };
+
+// export type AuthenticationRefreshResponse = Omit<
+//   AuthenticationSuccessResponse,
+//   'user'
+// >;
+
+// // Authorization
+// export interface AuthorizationTokenPayload extends JwtPayload {
+//   userId: string;
+//   email: string;
+//   role?: 'user' | 'admin';
+// }
