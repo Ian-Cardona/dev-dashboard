f49275ff06af7b411f56d482cbe01c5779ce781d
Ian Cardona <iancardona.dev@gmail.com>
2025-08-14 22:49:47 +0800

    refactor(refreshToken): remove exposed endpoints and obsolete files

--- packages/api/src/types/refreshToken.type.ts
+++ packages/api/src/types/refreshToken.type.ts
@@ -1,8 +1,13 @@
-export interface RefreshToken {
-  userId: string;
-  refreshTokenId: string;
-  refreshTokenHash: string;
-  expiresAt: string;
-  createdAt: string;
-  revoked: boolean;
-}
+import z from 'zod';
+import { refreshTokenSchema } from '../schema/refreshToken.schema';
+
+export type RefreshToken = z.infer<typeof refreshTokenSchema>;
+
+// export interface RefreshToken {
+//   userId: string;
+//   refreshTokenId: string;
+//   refreshTokenHash: string;
+//   expiresAt: string;
+//   createdAt: string;
+//   revoked: boolean;
+// }
