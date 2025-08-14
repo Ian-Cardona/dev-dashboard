6988e09fe3bbe9554adf074cfdf49dc6319478a3
Ian Cardona <iancardona.dev@gmail.com>
2025-08-14 22:49:47 +0800

    refactor(utils): update JWT utils for new auth flow

--- packages/api/src/utils/jwt.utils.ts
+++ packages/api/src/utils/jwt.utils.ts
@@ -1,18 +1,15 @@
-import jwt, { JwtPayload, SignOptions, VerifyOptions } from 'jsonwebtoken';
+import jwt, { SignOptions, VerifyOptions } from 'jsonwebtoken';
 import { ENV } from '../config/env_variables';
+import { AuthorizationTokenPayload } from '../types/auth.type';
+import { Request } from 'express';
 
-export interface AuthTokenPayload extends JwtPayload {
-  userId: string;
-  email: string;
-}
-
-export const generateJWT = (payload: AuthTokenPayload): string => {
+export const generateJWT = (payload: AuthorizationTokenPayload): string => {
   if (!ENV.JWT_SECRET) {
     throw new Error('JWT_SECRET is not defined in environment variables');
   }
 
   const signOptions: SignOptions = {
-    expiresIn: '30m',
+    expiresIn: '30m', // TODO: Use env variable for this
     algorithm: 'HS256',
     issuer: ENV.APP_NAME,
     audience: ENV.CLIENT_APP_NAME,
@@ -21,7 +18,7 @@ export const generateJWT = (payload: AuthTokenPayload): string => {
   return jwt.sign(payload, ENV.JWT_SECRET, signOptions);
 };
 
-export const verifyJWT = (token: string): AuthTokenPayload => {
+export const verifyJWT = (token: string): AuthorizationTokenPayload => {
   if (!ENV.JWT_SECRET) {
     throw new Error('JWT_SECRET is not defined in environment variables');
   }
@@ -32,5 +29,24 @@ export const verifyJWT = (token: string): AuthTokenPayload => {
     audience: ENV.CLIENT_APP_NAME,
   };
 
-  return jwt.verify(token, ENV.JWT_SECRET, verifyOptions) as AuthTokenPayload;
+  return jwt.verify(
+    token,
+    ENV.JWT_SECRET,
+    verifyOptions
+  ) as AuthorizationTokenPayload;
+};
+
+export const extractBearerToken = (req: Request): string => {
+  const authHeader = req.headers['authorization'];
+  if (!authHeader || Array.isArray(authHeader))
+    throw new Error('No authorization header');
+
+  const parts = authHeader.split(' ');
+  if (parts.length !== 2) throw new Error('Invalid authorization header');
+
+  const [scheme, token] = parts;
+  if (!/^Bearer$/i.test(scheme))
+    throw new Error('Invalid authorization header');
+
+  return token;
 };
