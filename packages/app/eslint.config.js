5d35eece67d800089bbac5c54248ee261cf9a882
Ian Cardona <iancardona.dev@gmail.com>
2025-08-16 23:07:22 +0800

    feat(app): initial frontend setup with Vite, Tailwind, axios, and TanStack Query

---
+++ packages/app/eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+]);
