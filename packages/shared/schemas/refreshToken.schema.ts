f1c59f339567efcefc50ce573ad5e9c302d53aa7
Ian Cardona <iancardona.dev@gmail.com>
2025-08-21 23:51:08 +0800

    refactor(api): implement token tombstoning and improve refresh logic

--- packages/shared/schemas/refreshToken.schema.ts
+++ packages/shared/schemas/refreshToken.schema.ts
@@ -7,6 +7,11 @@ export const refreshTokenSchema = z.object({
   expiresAt: z.iso.datetime({ message: 'Invalid ISO datetime' }),
   createdAt: z.iso.datetime({ message: 'Invalid ISO datetime' }),
   revoked: z.boolean({ message: 'Invalid boolean value' }),
+  revokedAt: z.iso
+    .datetime({ message: 'Invalid ISO datetime' })
+    .nullish()
+    .default(null)
+    .optional(),
 });
 
 export const refreshTokenCreateSchema = refreshTokenSchema
@@ -23,4 +28,8 @@ export const refreshTokenCreateSchema = refreshTokenSchema
         message: 'expiresAt must be a future date',
       }),
     revoked: z.boolean({ message: 'Invalid boolean value' }).default(false),
+    revokedAt: z.iso
+      .datetime({ message: 'Invalid ISO datetime' })
+      .nullish()
+      .default(null),
   });
