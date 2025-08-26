6ce01516efb6e41922ce78e5339ce5db0e125321
Ian Cardona <iancardona.dev@gmail.com>
2025-08-26 22:23:18 +0800

    feat(vscode): improve API integration and scanning logic

--- packages/vscode-extension/out/api.js
+++ packages/vscode-extension/out/api.js
@@ -1,25 +1,28 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.protectedClient = void 0;
-const axios_1 = __importDefault(require("axios"));
+const axios_1 = __importDefault(require('axios'));
 // const baseURL = process.env.API_URL || 'http://localhost:3000';
 const baseURL = 'http://localhost:3000';
 exports.protectedClient = axios_1.default.create({
-    baseURL,
-    headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-    },
-    withCredentials: true,
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+  withCredentials: true,
 });
-exports.protectedClient.interceptors.request.use((config) => {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1NmU4MDA3MC04OTg1LTQ0ODMtYmQwNC0wMWY4ODI4MzJhYzUiLCJlbWFpbCI6ImlhbmNhcmRvbmEwMDFAZ21haWwuY29tIiwiaXNBY3RpdmUiOnRydWUsImlhdCI6MTc1NjEzMzY4MCwiZXhwIjoxNzU2MTM1NDgwLCJhdWQiOiJEZXZEYXNoYm9hcmRVSSIsImlzcyI6IkRldkRhc2hib2FyZCJ9.v0vp7KaLGG2v0Pl8T3mq5B7-azWIdD-PUsxfsh1rt9U';
-    if (token) {
-        config.headers = config.headers ?? {};
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+exports.protectedClient.interceptors.request.use(config => {
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1NmU4MDA3MC04OTg1LTQ0ODMtYmQwNC0wMWY4ODI4MzJhYzUiLCJlbWFpbCI6ImlhbmNhcmRvbmEwMDFAZ21haWwuY29tIiwiaXNBY3RpdmUiOnRydWUsImlhdCI6MTc1NjIxNDgxOCwiZXhwIjoxNzU2MjE2NjE4LCJhdWQiOiJEZXZEYXNoYm9hcmRVSSIsImlzcyI6IkRldkRhc2hib2FyZCJ9.OfVIxhtChm83YD8E7-PrPdEhHY79a0c1JdNsYTWFQEA';
+  if (token) {
+    config.headers = config.headers ?? {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
