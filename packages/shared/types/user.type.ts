7aee292d78b55b9e518b5ada22b8b9efa4b294ab
Ian Cardona <iancardona.dev@gmail.com>
2025-08-27 23:30:46 +0800

    chore(shared): remove deprecated schemas and types, update configs

--- packages/shared/types/user.type.ts
+++
@@ -1,5 +0,0 @@
-import z from 'zod';
-import { userSchema, userResponsePublicSchema } from '../schemas/user.schema';
-
-export type User = z.infer<typeof userSchema>;
-export type UserResponsePublic = z.infer<typeof userResponsePublicSchema>;
