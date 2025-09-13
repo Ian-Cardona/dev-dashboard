ad1db1dfb984405c72a818d53d5dec7a1471a38e
Ian Cardona <iancardona.dev@gmail.com>
2025-09-13 21:04:40 +0800

    refactor(api): cleanup codebase

--- packages/vscode-extension/src/apis/api-keys-api.ts
+++ packages/vscode-extension/src/apis/api-keys-api.ts
@@ -4,7 +4,7 @@ export const apiKeysApi = {
   check: async (key?: string) => {
     const headers = key ? { Authorization: `Bearer ${key}` } : {};
 
-    // FIXME: Make this dynamic API endpoint
+    // FIXME: Add to env variables
     const response = await axios.get('/api-keys/check', {
       baseURL: 'http://localhost:3000/v1',
       headers: {
