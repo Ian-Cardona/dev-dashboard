a3cfad03977d1c92564e3becdeb756376b8eb44f
Ian Cardona <iancardona.dev@gmail.com>
2025-11-17 23:50:06 +0800

    Fix use of runtime config for cookies and api url

--- packages/app/src/utils/document/oauthCookies.ts
+++ packages/app/src/utils/document/oauthCookies.ts
@@ -1,16 +1,9 @@
-// TODO: setup as config
-export const OAUTH_SUCCESS_COOKIE_KEYS = {
-  provider: 'gh_o_p',
-  id: 'gh_o_i',
-  login: 'gh_o_l',
-  enc: 'gh_o_enc',
-};
+import {
+  getOAuthErrorCookieKeys,
+  getOAuthSuccessCookieKeys,
+  getRegInitCookieKeys,
+} from '../configs/envConfig';
 
-export const OAUTH_ERROR_COOKIE_KEYS = {
-  error: 'gh_o_e',
-};
-
-export const REG_INIT_COOKIE_KEYS = {
-  registration_token: 'regintkn',
-  registration_id: 'reginid',
-};
+export const OAUTH_SUCCESS_COOKIE_KEYS = getOAuthSuccessCookieKeys();
+export const OAUTH_ERROR_COOKIE_KEYS = getOAuthErrorCookieKeys();
+export const REG_INIT_COOKIE_KEYS = getRegInitCookieKeys();
