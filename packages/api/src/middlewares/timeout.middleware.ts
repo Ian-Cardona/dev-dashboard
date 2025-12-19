b6fc72c57d83a12620ae65ca15aed7f1011bb521
Ian Cardona <iancardona.dev@gmail.com>
2025-12-19 23:49:48 +0800

    fix(rate-limit): refactor timeout middleware to be a function rather than factory

--- packages/api/src/middlewares/timeout.middleware.ts
+++ packages/api/src/middlewares/timeout.middleware.ts
@@ -2,44 +2,46 @@ import { sendError } from '../utils/api.utils';
 import { NextFunction, Request, Response } from 'express';
 import { logger } from 'src/utils/logger.utils';
 
-const DEFAULT_TIMEOUT_MS = 30000;
+const TIMEOUT_MS = 30000;
 
-export const timeoutMiddleware = (timeoutMs: number = DEFAULT_TIMEOUT_MS) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    let timeoutOccurred = false;
+export const timeoutMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let timeoutOccurred = false;
 
-    const timer = setTimeout(() => {
-      timeoutOccurred = true;
+  const timer = setTimeout(() => {
+    timeoutOccurred = true;
 
-      if (!res.headersSent) {
-        logger.warn('Request timeout', {
-          method: req.method,
-          path: req.originalUrl,
-          ip: req.ip,
-          timeoutMs,
-        });
+    if (!res.headersSent) {
+      logger.warn('Request timeout', {
+        method: req.method,
+        path: req.originalUrl,
+        ip: req.ip,
+        timeoutMs: TIMEOUT_MS,
+      });
 
-        sendError(
-          res,
-          'Request Timeout',
-          'The request took too long to process',
-          408
-        );
-      }
-    }, timeoutMs);
+      sendError(
+        res,
+        'Request Timeout',
+        'The request took too long to process',
+        408
+      );
+    }
+  }, TIMEOUT_MS);
 
-    res.on('finish', () => {
-      clearTimeout(timer);
-    });
+  res.on('finish', () => {
+    clearTimeout(timer);
+  });
 
-    res.on('close', () => {
-      clearTimeout(timer);
-    });
+  res.on('close', () => {
+    clearTimeout(timer);
+  });
 
-    Object.defineProperty(res, 'timedOut', {
-      get: () => timeoutOccurred,
-    });
+  Object.defineProperty(res, 'timedOut', {
+    get: () => timeoutOccurred,
+  });
 
-    next();
-  };
+  next();
 };
